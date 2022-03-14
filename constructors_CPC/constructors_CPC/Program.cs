dde00c12706f748e700166fb1e6bab851f3d6cbe
cxii112 <lyakhovoy.d@ya.ru>
2022-03-14 21:28:42 +0500

    СРС по конструкторам
    Пока без комментариев

---
+++ constructors_CPC/constructors_CPC/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace constructors_CPC
+{
+    /**
+     * Вариант №4
+     * @author Дмитрий Ляховой ММТ-2-2021
+     */
+    internal sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант №4");
+            Console.Write("Введите координаты начальной точки");
+        }
+    }
+
+    internal class Vector
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        // Стандартный конструктор
+        Vector()
+        {
+            _x = 0;
+            _y = 0;
+            _z = 0;
+        }
+
+        Vector(int xStart, int yStart, int zStart,
+               int xEnd,   int yEnd,   int zEnd)
+        {
+            _x = xEnd - xStart;
+            _y = yEnd - yStart;
+            _z = zEnd - zStart;
+        }
+
+        public Vector(int x, int y, int z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public Vector Subtract(Vector v)
+        {
+            int a = this._x - v._x;
+            int b = this._y - v._y;
+            int c = this._z - v._z;
+            return new Vector(a, b, c);
+        }
+    }
+}
---
+++ constructors_CPC/constructors_CPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("constructors_CPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("constructors_CPC")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6A5E052C-8D89-481C-B9B1-A9CF2498E2E8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
