4c1ff4035261cbb07cf211cfe544c4f90035f362
cxii112 <lyakhovoy.d@ya.ru>
2022-03-28 16:51:21 +0500

    СРС по массивам 2

---
+++ arrays2_CPC/arrays2_CPC/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace arrays2_CPC
+{
+    /**
+     * @author Дмитрий Ляховой ММТ-2-2021
+     * Вариант №4
+     */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputPath = $@"{args[0]}";
+            string outputPath = $@"{args[1]}out.txt";
+            MASSIV_4 customArray = new MASSIV_4(10, inputPath, outputPath);
+            customArray.Read();
+            Console.WriteLine($"Полученный массив: {customArray}");
+            
+            customArray.Remove44();
+            Console.WriteLine($"Без 44: {customArray}");
+            
+            Console.Write("Введите элемент, после которого надо удалить оставшиеся ");
+            int borderNumber = Convert.ToInt32(Console.ReadLine());
+            customArray.RemoveAllAfter(borderNumber);
+            Console.WriteLine($"Обрезанный массив: {customArray}");
+            
+            customArray.MakeCirclePermutations();
+            Console.WriteLine($"Массив после цикличного сдвига: {customArray}");
+            
+            customArray.Write();
+        }
+        // Полученный массив: [44, 123, -34, 12523, 2532, 4574, -924, 340, -904, 1]
+        // Без 44: [123, -34, 12523, 2532, 4574, -924, 340, -904, 1]
+        // Введите элемент, после которого надо удалить оставшиеся 4574
+        // Обрезанный массив: [123, -34, 12523, 2532, 4574]
+        // Массив после цикличного сдвига: [-34, 12523, 2532, 4574, 123]
+
+        
+        // Полученный массив: [44, 123, -34, 12523, 2532, 4574, -924, 340, -904, 1]
+        // Без 44: [123, -34, 12523, 2532, 4574, -924, 340, -904, 1]
+        // Введите элемент, после которого надо удалить оставшиеся -924
+        // Обрезанный массив: [123, -34, 12523, 2532, 4574, -924]
+        // Массив после цикличного сдвига: [123, -34, 12523, 2532, 4574, -924]
+
+        
+        // Полученный массив: [44, 123, -34, 12523, 2532, 4574, -924, 340, -904, 1]
+        // Без 44: [123, -34, 12523, 2532, 4574, -924, 340, -904, 1]
+        // Введите элемент, после которого надо удалить оставшиеся 12523
+        // Обрезанный массив: [123, -34, 12523]
+        // Массив после цикличного сдвига: [123, -34, 12523]
+    }
+
+    // Задание
+    // Разработать отдельный класс MASSIV_4 для работы с одномерным массивом целочисленных элементов.
+    // Элементы класса:
+    //      Поля: N4 – размерность массива;
+    //      Методы:
+    //          * +++   заполнение элементов массива из файла (см. Пример 6);
+    //              предусмотрите наличие в файле значений элементов массива,
+    //              необходимых для реализации методов;
+    //          * +++   вывод массива на экран;
+    //          * +++   удаление всех элементов массива,
+    //              расположенных после элемента со значением К4,
+    //              введенного с клавиатуры;
+    //          * +++   удаление элемента массива со значением 44;
+    //          * +++   реализация циклического сдвига элементов массива влево на 6 элементов;
+    //          * +++   запись измененного массива в новый файл (см. Приложение 2).
+    // Модификаторы доступа для полей определите самостоятельно.
+    // Модификаторы доступа для методов должны обеспечить
+    // доступ ко всем методам из любого метода любого класса программы.
+    // Класс Program обязательно должен содержать объявление (описание) массива.
+
+    internal class MASSIV_4
+    {
+        public MASSIV_4(int n4, string inputFile, string outputFile = @"./out.txt")
+        {
+            N4 = n4;
+            InputFile = inputFile;
+            OutputFile = outputFile;
+            Numbers = new int[N4];
+        }
+
+        public void Read()
+        {
+            using (StreamReader inputStream = File.OpenText(InputFile))
+            {
+                Numbers = inputStream.ReadLine()
+                                     .Split(',')
+                                     .Take(N4)
+                                     .Select(numberString => int.Parse(numberString))
+                                     .ToArray();
+            }
+        }
+
+        public void Write()
+        {
+            using (StreamWriter outputStream = File.CreateText(OutputFile))
+            {
+                var @join = string.Join(",", Numbers);
+                outputStream.Write(@join);
+            }
+        }
+
+        public void RemoveAllAfter(int after)
+        {
+            Numbers = Numbers.TakeWhile(number => number != after)
+                             .Append(after)
+                             .ToArray();
+        }
+
+        public void Remove44()
+        {
+            Numbers = Numbers.Where(number => number != 44)
+                             .ToArray();
+        }
+
+        public void MakeCirclePermutations()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Numbers = Numbers.Skip(1).Append(Numbers[0]).ToArray();
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", Numbers)}]";
+        }
+
+        public int N4 { get; }
+        public string InputFile { get; }
+        public string OutputFile { get; }
+
+        private int[] Numbers { get; set; }
+    }
+}
---
+++ arrays2_CPC/arrays2_CPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("arrays2_CPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("arrays2_CPC")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5D45033B-1AAF-4417-9814-FB9F445B54AE")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
