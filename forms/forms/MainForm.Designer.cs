8289892a5c99d81d1e99b6a4f925a1fcdc7529be
cxii112 <lyakhovoy.d@ya.ru>
2022-04-18 22:48:18 +0500

    Графические приложения ауд

---
+++ forms/forms/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.countButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rebootButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(100, 61);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(52, 25);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(131, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(34, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(46, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(108, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(171, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Location = new System.Drawing.Point(196, 35);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(78, 20);
+            this.resultLabel.TabIndex = 7;
+            // 
+            // countButton
+            // 
+            this.countButton.Location = new System.Drawing.Point(13, 60);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(81, 25);
+            this.countButton.TabIndex = 8;
+            this.countButton.Text = "Расчитать";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(13, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "a";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(73, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "b";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(131, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "c";
+            // 
+            // rebootButton
+            // 
+            this.rebootButton.Location = new System.Drawing.Point(164, 62);
+            this.rebootButton.Name = "rebootButton";
+            this.rebootButton.Size = new System.Drawing.Size(59, 22);
+            this.rebootButton.TabIndex = 12;
+            this.rebootButton.Text = "Reboot";
+            this.rebootButton.UseVisualStyleBackColor = true;
+            this.rebootButton.Click += new System.EventHandler(this.rebootButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 102);
+            this.Controls.Add(this.rebootButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.countButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exitButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button rebootButton;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Button countButton;
+
+        private System.Windows.Forms.Label resultLabel;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button exitButton;
+
+        #endregion
+    }
+}
---
+++ forms/forms/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double a = double.Parse(this.textBox1.Text);
+                double b = double.Parse(this.textBox2.Text);
+                int c = int.Parse(this.textBox3.Text);
+                resultLabel.Text = (a + b + c).ToString();
+            }
+            catch (FormatException)
+            {
+                
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void rebootButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
