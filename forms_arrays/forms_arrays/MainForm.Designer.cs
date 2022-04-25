9b8f019ec72f1ae3e41be4d027424b2412dd29c7
cxii112 <lyakhovoy.d@ya.ru>
2022-04-25 16:50:27 +0500

    Ауд Формы 2

---
+++ forms_arrays/forms_arrays/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace forms_arrays
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.updateMatrixButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 95);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(783, 170);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // updateMatrixButton
+            // 
+            this.updateMatrixButton.Location = new System.Drawing.Point(247, 26);
+            this.updateMatrixButton.Margin = new System.Windows.Forms.Padding(7);
+            this.updateMatrixButton.Name = "updateMatrixButton";
+            this.updateMatrixButton.Size = new System.Drawing.Size(314, 55);
+            this.updateMatrixButton.TabIndex = 1;
+            this.updateMatrixButton.Text = "Обновить матрицу";
+            this.updateMatrixButton.UseVisualStyleBackColor = true;
+            this.updateMatrixButton.Click += new System.EventHandler(this.updateMatrixButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(818, 646);
+            this.Controls.Add(this.updateMatrixButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button updateMatrixButton;
+    }
+}
+
---
+++ forms_arrays/forms_arrays/MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forms_arrays
+{
+    public partial class MainForm : Form
+    {
+        private int[,] _matrix;
+        private int N;
+        private int M;
+        public MainForm()
+        {
+            InitializeComponent();
+            N = 5;
+            M = 5;
+        }
+
+        private int[,] InitMatrix(int n, int m)
+        {
+            Random random = new Random();
+            int[,] matrix = new int[n, m];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = random.Next(0, 10);
+                }
+            }
+
+            return matrix;
+        }
+
+        private void UpdateMatrix()
+        {
+            _matrix = InitMatrix(N, M);
+            dataGridView1.RowCount = M;
+            dataGridView1.ColumnCount = N;
+            // dataGridView1.DataSource = MatrixToRow();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = _matrix[i, j].ToString();
+                    dataGridView1.UpdateCellValue(i, j);
+                }
+            }
+        }
+
+        private int[] MatrixToRow()
+        {
+            int[] row = new int[N * M];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    row[i * N + j] = _matrix[i, j];
+                }
+            }
+
+            return row;
+        }
+
+        private void updateMatrixButton_Click(object sender, EventArgs e)
+        {
+            UpdateMatrix();
+        }
+    }
+}
